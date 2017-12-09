40c00804ff2180d69daf6930c976c091866534a8
FloBra <flobra@MacBook-Air-van-FloBra.local>
2017-12-09 19:59:15 +0100

    Initial Commit

---
+++ Valuta Omrekenen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Valuta Omrekenen
+//
+//  Created by FloBra on 09-12-17.
+//  Copyright © 2017 FloBra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
