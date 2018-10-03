22fedaeae09f0dfd1a55ee51c80d0f27f082d116
FloBra <flobra@MacBook-Air-van-FloBra.local>
2018-10-03 21:05:30 +0200

    first commit

---
+++ Valuta Omrekenen/ViewController3.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController3.swift
+//  Valuta Omrekenen
+//
+//  Created by FloBra on 20-08-18.
+//  Copyright © 2018 FloBra. All rights reserved.
+//
+
+import Foundation
---
+++ Valuta Omrekenen/ViewControllerAllCurrencies.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Valuta Omrekenen
+//
+//  Created by FloBra on 10-06-18.
+//  Copyright © 2018 FloBra. All rights reserved.
+//
+
+import Foundation
